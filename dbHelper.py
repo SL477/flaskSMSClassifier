eae0da971b4867a22145997c744d86a494d70909
Tom Fishwick <fishwickt@gmail.com>
2021-05-03 16:03:16 +0100

    Got database setup and script to recreate it

---
+++ dbHelper.py
@@ -0,0 +1,54 @@
+import sqlite3
+from sqlite3 import Error
+
+def sql_connection():
+    """
+    This is to get the database connection
+    """
+    try:
+        return sqlite3.connect('data.db')
+    except Error:
+        print(Error)
+
+def executeSql(con, sqlText):
+    """
+    This is to execute arbitary SQL commands
+    """
+    cursor = con.cursor()
+    cursor.execute(sqlText)
+    con.commit()
+
+def firstTimeSetup():
+    """
+    This is to run the first time setups
+
+    messages
+    Columns:
+    id
+    msg
+    type
+
+    types
+    Columns:
+    id
+    desc
+    """
+    con = sql_connection()
+    messageTableSql = "CREATE TABLE messages(id integer PRIMARY KEY, msg text, type integer);"
+    executeSql(con, "DROP TABLE IF EXISTS messages;")
+    executeSql(con, messageTableSql)
+    typesTableSql = "CREATE TABLE types(id bit, desc text);"
+    executeSql(con, "DROP TABLE IF EXISTS types;")
+    executeSql(con, typesTableSql)
+
+    typesRowsSQL = "INSERT INTO types (id, desc) VALUES (0, 'spam'), (1,'ham')"
+    executeSql(con, typesRowsSQL)
+    con.close()
+
+def insertDataIntoMessages(con, msgString, typeBool):
+    """
+    This is to insert a line of data into the messages table with its classification
+    """
+    cursor = con.cursor()
+    cursor.execute("INSERT INTO messages (msg, type) VALUES (?,?)", (msgString, typeBool))
+    con.commit()
---
+++ setup.py
@@ -0,0 +1,23 @@
+# This is to run the first time setup
+import dbHelper
+import pandas as pd
+from os import path
+
+# Get the data from the TSV files
+def getTSVDataFrame(fileName):
+    """
+    This gets the dataframe from the file name
+    """
+    df_ret = pd.read_csv(fileName, header=None, sep='\t', names=['type','msg'])
+    df_ret['type'] = df_ret['type'] == 'ham'
+    return df_ret
+
+if __name__ == '__main__':
+    dbHelper.firstTimeSetup()
+    df = getTSVDataFrame(path.join('data', 'train-data.tsv'))
+    con = dbHelper.sql_connection()
+    #print(df.head())
+    for index, row in df.iterrows():
+        #print(row['msg'],row['type'])
+        dbHelper.insertDataIntoMessages(con, row['msg'], row['type'])
+    con.close()
