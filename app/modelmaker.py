b8c725ead070bbf3d5657c85f1730b4653b699f5
Tom Fishwick <fishwickt@gmail.com>
2021-09-09 15:49:51 +0100

    Started saving the model

--- app/modelmaker.py
+++ app/modelmaker.py
@@ -10,7 +10,7 @@ from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.linear_model import SGDClassifier
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-
+import pickle
 # Split to X and y
 def splitXAndY(filePath):
     ''''
@@ -38,12 +38,13 @@ print(y_train.head())
 
 # Classify
 #svm = SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None)
-
+'''
 pipeline = Pipeline([
     ('vect', CountVectorizer()),
     ('tfidf', TfidfTransformer(use_idf=False)),
     ('svm', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None))
     ])
+
 
 pipeline.fit(X_train, y_train)
 
@@ -52,3 +53,46 @@ y_pred = pipeline.predict(X_test)
 print('accuracy', accuracy_score(y_test, y_pred))
 print('Classification report', classification_report(y_test, y_pred))
 print('confusion matrix', confusion_matrix(y_test, y_pred))
+'''
+
+#%% Grid search cv
+# need to beat 0.9741379310344828% accurancy
+'''
+from sklearn.model_selection import GridSearchCV
+pipeline = Pipeline([
+    ('vect', CountVectorizer()),
+    ('tfidf', TfidfTransformer(use_idf=False)),
+    ('svm', SGDClassifier(loss='hinge', penalty='l2', alpha=1e-3, random_state=42, max_iter=5, tol=None))
+    ])
+
+parameters = {
+    'vect__ngram_range': [(1, 1), (1, 2)],
+    'tfidf__use_idf': (True, False),
+    'svm__alpha': [1e-1, 1e-2, 1e-3],
+    'svm__max_iter': [5, 6, 7]
+    }
+
+gridSearchSVM = GridSearchCV(pipeline, parameters, cv=5, n_jobs=-1)
+gridSearchSVM.fit(X_train, y_train)
+
+print('best score', gridSearchSVM.best_score_)
+print('best params', gridSearchSVM.best_params_)
+'''
+
+#%% Final pipeline
+pipeline = Pipeline([
+    ('vect', CountVectorizer(ngram_range=(1,2))),
+    ('tfidf', TfidfTransformer(use_idf=False)),
+    ('svm', SGDClassifier(loss='hinge', penalty='l2', alpha=0.001, random_state=42, max_iter=5, tol=None))
+    ])
+pipeline.fit(X_train, y_train)
+
+y_pred = pipeline.predict(X_test)
+
+print('accuracy', accuracy_score(y_test, y_pred))
+print('Classification report', classification_report(y_test, y_pred))
+print('confusion matrix', confusion_matrix(y_test, y_pred))
+
+#%% Save model
+filehandler = open("model.obj", "wb")
+pickle.dump(pipeline, filehandler)
